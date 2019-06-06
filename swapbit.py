3b1dd73e3f41e235a9799e7aa4fcd9a43243c41b
atchu1 <atchayaraj18@gmail.com>
2019-06-06 12:27:29 +0530

    Create swapbit.py

---
+++ swapbit.py
@@ -0,0 +1,9 @@
+n1 = int(input('Enter 1 Number: '))
+n2 = int(input('Enter 2 Number: '))
+print("Value of num1 before swapping: ", n1)
+print("Value of num2 before swapping: ", n2)
+n1= n1*n2
+n2 = n1//n2
+n1 = n1//n2
+print("Value of num1 after swapping: ", n1)
+print("Value of num2 after swapping: ", n2)
