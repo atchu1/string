c53f797481c232601b7091b623dc48e00b26b995
atchu1 <atchayaraj18@gmail.com>
2019-06-06 12:20:43 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,9 +1,9 @@
-num1 = input('Enter First Number: ')
-num2 = input('Enter Second Number: ')
-print("Value of num1 before swapping: ", num1)
-print("Value of num2 before swapping: ", num2)
-temp = num1
-num1 = num2
-num2 = temp
-print("Value of num1 after swapping: ", num1)
-print("Value of num2 after swapping: ", num2)
+n1 = input('Enter 1 Number: ')
+n2 = input('Enter 2 Number: ')
+print("Value of num1 before swapping: ", n1)
+print("Value of num2 before swapping: ", n2)
+temp = n1
+n1 = n2
+n2 = temp
+print("Value of num1 after swapping: ", n1)
+print("Value of num2 after swapping: ", n2)
