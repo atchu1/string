6ecc0aaf6ee2deb0d53f59eca80cb519facf7890
atchu1 <atchayaraj18@gmail.com>
2019-06-06 14:29:18 +0530

    Create avg.py

---
+++ avg.py
@@ -0,0 +1,7 @@
+num=input("Enter how many number:")
+total_sum = 0
+for n in range(num):
+numbers = float(input('Enter number : '))
+total_sum += numbers
+avg=total_sum/num
+print('Average of ', num, ' numbers is :', avg)
