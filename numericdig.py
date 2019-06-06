4b2adf548fc8359ed4020453ba571e7cb05899b3
atchu1 <atchayaraj18@gmail.com>
2019-06-06 13:50:06 +0530

    Update numericdig.py

--- numericdig.py
+++ numericdig.py
@@ -1,6 +1,6 @@
-n=int(input("Enter number:"))
+n1=int(input("Enter number:"))
 count=0
 while(n>0):
     count=count+1
-    n=n//10
+    n1=n1//10
 print("The number of digits in the number are:",count)
