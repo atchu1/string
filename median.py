2d8e9bc022a4013b79dde5d5b84c1ad7b31630c9
atchu1 <atchayaraj18@gmail.com>
2019-06-07 15:27:45 +0530

    Update median.py

--- median.py
+++ median.py
@@ -1,7 +1,7 @@
 def median(p):
-      q=sorted(p)
-      return q[(len(p)//2)]
-r=int(input())
+      a=sorted(p)
+      return a[(len(p)//2)]
+m=int(input())
 p=input()
 p=p.split(" ")
 p=list(map(int,p))
