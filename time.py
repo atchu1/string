a47664f29a7128f029e7b4d54f2b1f1916ac01af
atchu1 <atchayaraj18@gmail.com>
2019-06-07 15:37:31 +0530

    Update time.py

--- time.py
+++ time.py
@@ -1,9 +1,4 @@
-time = int(input("Input time in minutes: "))
-day = time // (24 * 3600)
-time = time % (24 * 3600)
-hour = time // 3600
-time %= 3600
-minutes = time // 60
-time %= 60
-minutes = time
-print("h:m-> %d:%d" % (hour, minutes))
+M=int(input())
+A=M//60
+P=M%60
+print(A,P)
