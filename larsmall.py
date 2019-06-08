c74b2436d7534e99f7d1ca063cfd5b4a459bdd68
atchu1 <atchayaraj18@gmail.com>
2019-06-08 14:37:47 +0530

    Update larsmall.py

--- larsmall.py
+++ larsmall.py
@@ -1,7 +1,4 @@
-List = []
-Number = int(input("Enter  the Total Number of List Elements:"))
-for i in range(1, Number + 1):
- value = int(input("Please enter the Value of %d Element :"%i))
-List.append(value)
-print("The Smallest Element in  List is :", min(List))
-print("The Largest Element in List is : ", max(List))
+num=int(input())
+l=list(map(int,(input().split())))
+l.sort()
+print(min(l),max(l))
