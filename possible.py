465ce0264c56af7a94d568967d930fe8c2ee4d3c
atchu1 <atchayaraj18@gmail.com>
2019-07-10 16:15:07 +0530

    Update possible.py

--- possible.py
+++ possible.py
@@ -1,8 +1,8 @@
 r,s=input().split()
 t1=abs(len(r)-len(s))
 for i in range(len(r)):
-if len(s)==1 and s[i] in r:
- break
-if r[i]!=s[i]:
- t1=t1+1
- print(t1)
+  if len(s)==1 and s[i] in r:
+    break
+  if r[i]!=s[i]:
+    t1=t1+1
+print(t1)
